c3f8229bd1aaddab63bb3e2e23a1ce55347ecbaa
Hayse3101 <lolartem999@gmail.com>
2024-06-25 15:19:10 +0300

    commit and push - 25.06.2024 15:19

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-def make_title(func):
-    def warpped():
-        return func().capitalize().replace(',', '')
-    return warpped
 
-
-@make_title
-def hi():
-    return 'hello, world!'
-
-
-print(hi())
